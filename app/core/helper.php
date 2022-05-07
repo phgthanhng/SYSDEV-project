96976c786a258c543ae216fe01fff8663d07d2d8
Hawad Ahmad <hawadjan.1@gmail.com>
2022-05-07 13:01:27 -0400

    moved sendmail function to helper.php

--- app/core/helper.php
+++ app/core/helper.php
@@ -1,11 +1,12 @@
 <?php
+  use PHPMailer\PHPMailer\PHPMailer;
 
     session_start();
 
     function isLoggedIn() {
       return isset($_SESSION['admin_id']);
     }
-    
+
     /*
      * Validates the email 
      */
@@ -21,4 +22,46 @@
 
         return false;
     }
-?>
+
+    /*
+     * Sends reset password to email 
+     */
+    function sendmail(){
+      $token = bin2hex(random_bytes(20));
+
+      $name = "ShishaShop";  // Name of your website or yours
+      $to = "vaniercompsci@gmail.com";  // mail of receiver  // for testing purpose only login to this one and send to self
+      $subject = "Reset password";
+      $body = "<a href = 'http://localhost/Sysdev-project/Admin/changePassword?token=".$token."'>Reset password</a>";
+      $from = "vaniercompsci@gmail.com";  // you mail
+      $password = "sysdev123";  // your mail password
+
+      $mail = new PHPMailer();
+
+      //SMTP Settings
+      $mail->isSMTP();
+      // $mail->SMTPDebug = 3;  Keep It commented this is used for debugging                          
+      $mail->Host = "smtp.gmail.com"; // smtp address of your email
+      $mail->SMTPAuth = true;
+      $mail->Username = $from;
+      $mail->Password = $password;
+      $mail->Port = 587;  // port
+      $mail->SMTPSecure = "tls";  // tls or ssl
+      $mail->smtpConnect([
+      'ssl' => [
+          'verify_peer' => false,
+          'verify_peer_name' => false,
+          'allow_self_signed' => true
+          ]
+      ]);
+
+      //Email Settings
+      $mail->isHTML(true);
+      $mail->setFrom($from, $name);
+      $mail->addAddress($to); // enter email address whom you want to send
+      $mail->Subject = ("$subject");
+      $mail->Body = $body;
+
+      if ($mail->send())
+          return $token;
+  }
