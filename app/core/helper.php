e92819d2449ed3c865dfd6079f2b6fdfb1b678ce
Hawad Ahmad <hawadjan.1@gmail.com>
2022-05-07 12:55:57 -0400

    moved validate_email function to helper.php

--- app/core/helper.php
+++ app/core/helper.php
@@ -5,4 +5,20 @@
     function isLoggedIn() {
       return isset($_SESSION['admin_id']);
     }
+    
+    /*
+     * Validates the email 
+     */
+    function validate_email($email)
+    {
+        if (isset($email)) {
+            $email = trim($email);
+            $sanitized_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+            $validated_email = filter_var($sanitized_email, FILTER_VALIDATE_EMAIL);
+
+            return $validated_email;
+        }
+
+        return false;
+    }
 ?>
